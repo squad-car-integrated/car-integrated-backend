54bd0986d8548c0339c0bede54edca93e8a1146f
natan borges <natanmelo5@hotmail.com>
2023-09-18 13:07:17 -0300

    Ajustes no caminho

--- test/setup-e2e.ts
+++ test/setup-e2e.ts
@@ -2,6 +2,7 @@ import 'dotenv/config'
 import { PrismaClient } from '@prisma/client'
 import { randomUUID } from 'crypto'
 import { execSync } from 'node:child_process'
+import { join } from 'path'
 
 const prisma = new PrismaClient()
 
@@ -17,7 +18,11 @@ const schemaId = randomUUID()
 beforeAll(async () => {
   const databaseUrl = generateUniqueDatabaseUrl(schemaId)
   process.env.DATABASE_URL = databaseUrl
-  execSync('npx prisma migrate deploy')
+  if(!process.env.APPDATA) {
+    throw new Error('Please provide a APPDATA enviroment variable')
+  }
+  const npxPath = join(process.env.APPDATA, 'npm', 'node_modules', 'npm', 'bin', 'npx.cmd');
+  execSync(`${npxPath} prisma migrate deploy`)
 })
 afterAll(async () => {
   await prisma.$executeRawUnsafe(`DROP SCHEMA IF EXISTS "${schemaId}" CASCADE`)
