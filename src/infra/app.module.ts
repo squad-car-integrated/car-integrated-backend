57a8d9240ade4f01258f108d963f203e0bd41d52
natan borges <natanmelo5@hotmail.com>
2023-12-07 15:32:32 -0300

    Import fix

--- src/infra/app.module.ts
+++ src/infra/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
-import { envSchema } from './env'
 import { AuthModule } from './auth/auth.module'
 import { HttpModule } from './http/http.module'
 import { EnvModule } from './env/env.module'
 import { EnvService } from './env/env.service'
+import { envSchema } from './env/env'
 @Module({
     imports: [
         ConfigModule.forRoot({
--- src/infra/main.ts
+++ src/infra/main.ts
@@ -1,8 +1,8 @@
 import { NestFactory } from '@nestjs/core'
 import { ConfigService } from '@nestjs/config'
-import { Env } from './env'
 import { AppModule } from './app.module'
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
+import { Env } from './env/env'
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule)
